4cc1bf06dd4c9d603db7c2f86a48b64fa2fc83b4
yinlianghui <yinlianghui@hotoa.com>
2017-05-10 13:41:22 +0800

    解决由于js文件缺失造成的导入脚本文件报错的问题。

--- package.js
+++ package.js
@@ -15,10 +15,19 @@ Package.onUse(function(api) {
 	api.addAssets('img/glyphicons-halflings-white.png', 'client');
 	api.addAssets('img/glyphicons-log-in.png', 'client');
 	api.addAssets('img/glyphicons-log-out.png', 'client');
+	
+	api.addAssets('plugins/thirdparty/jquery.sparkline.min.js', 'client');
+	api.addAssets('plugins/thirdparty/justgage.1.0.1.js', 'client');
+	api.addAssets('plugins/thirdparty/raphael.2.1.0.min.js', 'client');
+
 
 	api.addAssets('css/freeboard.min.css', 'client');
+	api.addAssets('js/freeboard.min.js', 'client');
 	api.addAssets('js/freeboard.thirdparty.min.js', 'client');
 	api.addAssets('js/freeboard_plugins.min.js', 'client');
+	api.addAssets('js/freeboard.plugins.min.js', 'client');
+	api.addAssets('index.html', 'client');
+
 	api.addAssets('assets/index.html', 'client');
 
 });
