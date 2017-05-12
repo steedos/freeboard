59a8d58e370a284ff1b98be9422dddcf54155961
zhusijia <zhusijia@hotoa.com>
2017-05-12 15:49:03 +0800

    调用接口

--- assets/index.js
+++ assets/index.js
@@ -3,6 +3,20 @@ head.js("js/freeboard_plugins.min.js",
 	function(){
 		$(function(){
 			// 需要根据url中dashboardId调用后台相关接口，返回dashboard脚本内容及编辑权限等
+			var dashboardId;
+			var reg = new RegExp("(^|&)dashboardId=([^&]*)(&|$)"); 
+			var r = window.location.search.substr(1).match(reg);
+			r != null ? dashboardId = unescape(r[2]) : dashboardId = null;
+			console.log(dashboardId);
+			if (dashboardId != null){
+				$.getJSON("/api/dashboard/"+dashboardId,function(data){
+					// freeboard.loadDashboard(data,function(){
+					// 	freeboard.setEditing(false);
+					// })
+					console.log("-------------get--------------")
+					console.log(data)
+				})
+			}
 			// 根据接口返回的编辑权限执行freeboard.initialize函数
 			freeboard.initialize(true);
 			// 根据接口返回的脚本内容执行freeboard.loadDashboard加载dashboard脚本
@@ -19,6 +33,10 @@ head.js("js/freeboard_plugins.min.js",
 					dashboardContent = JSON.stringify(freeboard.serialize());
 				}
 				//调用后台相关接口把dashboardContent保存到后台数据库
+				$.post("/api/dashboard/"+dashboardId,{freeboard:dashboardContent},function(data){
+					console.log("-------------post--------------")
+					console.log(data)
+				})
 			});
 		});
 	}
